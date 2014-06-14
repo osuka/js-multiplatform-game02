3c622912242f41a92665188d83c495d0ec5e5320
osuka <disposuka@gmail.com>
2014-06-14 17:58:21 +0100

    Updated gitignore

--- frameworks/runtime-src/proj.android/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-/Users/osuka/code/js-multiplatform-game02/frameworks/runtime-src/proj.android/gen/com/gatillos/js02/R.java \
- : /Users/osuka/code/js-multiplatform-game02/frameworks/runtime-src/proj.android/res/drawable-hdpi/icon.png \
-/Users/osuka/code/js-multiplatform-game02/frameworks/runtime-src/proj.android/res/drawable-ldpi/icon.png \
-/Users/osuka/code/js-multiplatform-game02/frameworks/runtime-src/proj.android/res/drawable-mdpi/icon.png \
-/Users/osuka/code/js-multiplatform-game02/frameworks/runtime-src/proj.android/res/values/strings.xml \
-/Users/osuka/code/js-multiplatform-game02/frameworks/runtime-src/proj.android/bin/AndroidManifest.xml \
